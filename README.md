ade6d56d1e3032d997556211c102751e404ad381
KDJDEV <kyledagman7@gmail.com>
2023-06-15 15:51:22 -0700

    updated for my original misunderstanding of the problem

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # 1d6 game
-Solution to this stats problem:
-You and your opponent play a game where you each take turns rolling 1d6 (a six sided die) and add the number rolled to your own running total. The first to reach 11 wins. You get to add +1 to each of your rolls. What is the probability that you will win if you go first? Second? What is the probability you will win if you have a 30% chance of going first?
+Code that finds the solution to this stats problem:
+You and your opponent play a game where you each take turns rolling 1d6 (a six sided die) and add the number rolled to your own running total. The first to reach 11 wins. You (but not your opponent) get to add +1 to each of your rolls. What is the probability that you will win if you go first? Second? What is the probability you will win if you have a 30% chance of going first?
 
 This is the final output of running the script:
 ```
-Final probability of win for P1: 42741667/60466176 ≈ 0.7068690270739132
-Final probability of win for P2: 17724509/60466176 ≈ 0.2931309729260868
+P1(W) = Final probability of winning for P1 = 1093853/1259712 ≈ 0.8683357783366357
+P2(W) = probability of winning for P2 = 165859/1259712 ≈ 0.1316642216633643
 
-Final probability of winning if you have a 30% chance of going first: 63074141/151165440 ≈ 0.4172523891704347
+By simply weighting these probabilities, you can find that if you have a 30% chance of going first:
+P(W) = 394123169/604661760 ≈ 0.6518076635109189
 ```
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from sympy import symbols, expand, Rational
+from sympy import symbols, expand
 
 x = symbols('x')
 
-rollPolynomial = x**2 + x**3 + x**4 + x**5 + x**6 + x**7 #The polynomial we multiply P1Polynomial or P2Polynomial by to get all possible sums
+"""
+Notice that the P1 and P2 RollPolynomials have different terms.
+This code currently calculates for if you go first, which is why I'm letting the P1 polynomial have the larger terms(for the +1).
+If you want to calculate for if you go second, switch the roll polynomials.
+"""
+P1RollPolynomial = x**2 + x**3 + x**4 + x**5 + x**6 + x**7 #The polynomial we multiply P1Polynomial by to get all possible sums
+P2RollPolynomial = x + x**2 + x**3 + x**4 + x**5 + x**6 #The polynomial we multiply P2Polynomial by to get all possible sums
 P1Polynomial = 1
 P2Polynomial = 1
 P1WinProb = 0
 P2WinProb = 0
 probToGetToThisPoint = 1
 
-def getPossibleSumsPolynomial(currentPolynomial):
+def getPossibleSumsPolynomial(currentPolynomial, rollPolynomial):
     expanded = expand(currentPolynomial * rollPolynomial)
     return expanded
 
@@ -29,13 +35,12 @@ def getProbWin(possibleSumsPolynomial): #assumes we made it to this point
         if (exponent >= 11):
             numWinningRolls += coefficient
         totalRolls += coefficient
-        #print(str(coefficient) + "x^" + str(exponent))
     return numWinningRolls / totalRolls
 
 turn = 1
 
 while True:
-    possibleSumsPolynomial = getPossibleSumsPolynomial(P1Polynomial)
+    possibleSumsPolynomial = getPossibleSumsPolynomial(P1Polynomial, P1RollPolynomial)
     turnWinProb = getProbWin(possibleSumsPolynomial)
     P1WinProb += probToGetToThisPoint * turnWinProb
     P1Polynomial = getLosePolynomial(possibleSumsPolynomial)
@@ -44,7 +49,7 @@ while True:
     print(f"Probability that they won this turn: {turnWinProb}")
     print(f"Probability that they have actually gotten to this point: {probToGetToThisPoint}\n")
 
-    possibleSumsPolynomial = getPossibleSumsPolynomial(P2Polynomial)
+    possibleSumsPolynomial = getPossibleSumsPolynomial(P2Polynomial, P2RollPolynomial)
     turnWinProb = getProbWin(possibleSumsPolynomial)
     P2WinProb += probToGetToThisPoint * turnWinProb
     P2Polynomial = getLosePolynomial(possibleSumsPolynomial)
@@ -56,8 +61,5 @@ while True:
     if (P1Polynomial == 0 or P2Polynomial == 0):
         print(f"Final probability of win for P1: {P1WinProb} ≈ {float(P1WinProb)}")
         print(f"Final probability of win for P2: {P2WinProb} ≈ {float(P2WinProb)} \n")
-
-        weightedProb = P1WinProb * Rational(3, 10) + P2WinProb * Rational(7,10)
-        print(f"Final probability of winning if you have a 30% chance of going first: {weightedProb} ≈ {float(weightedProb)}")
         break
     turn += 1
